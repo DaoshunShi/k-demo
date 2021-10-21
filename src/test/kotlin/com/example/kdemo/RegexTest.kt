56e877e3be0c4230a695a37b3451a659894cc137
ShiDaoshun <shidaoshun@seer-group.com>
2021-10-21 13:54:30 +0800

    正则提取字符串

--- src/test/kotlin/com/example/kdemo/RegexTest.kt
+++ src/test/kotlin/com/example/kdemo/RegexTest.kt
@@ -4,6 +4,11 @@ import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 fun main() {
+    // fun1()
+    fun2()
+}
+
+fun fun1() {
     val msg = "Duplicate entry '1234' for key 'wms_warehouse.UK_5vgfkcoe5gis7e8tclk0yydj3'"
     val pattern = "Duplicate entry '[\\u4E00-\\u9FA5A-Za-z0-9_]+' for key '[\\u4E00-\\u9FA5A-Za-z0-9_.]+'"
     
@@ -19,5 +24,20 @@ fun main() {
         while (m2.find()) {
             println(m2.group())
         }
+    }
+}
+
+fun fun2() {
+    val msg = "AB-123456-098765-0.1"
+    val pattern = "^AB-(\\w{6})-(\\w{6})-(\\d+.\\d+)\$"
+    
+    val r: Pattern = Pattern.compile(pattern, 1)
+    val m: Matcher = r.matcher(msg)
+    
+    if (m.matches()) {
+        println(m.group(0))
+        println(m.group(1))
+        println(m.group(2))
+        println(m.group(3))
     }
 }
